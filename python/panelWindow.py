3fc3b189c56050fec32574c7993585085f24f7eb
freshNfunky <felix@ftc-creative.de>
2012-09-04 14:07:54 +0200

    debug panel callbacks

--- python/panelWindow.py
+++ python/panelWindow.py
@@ -2,9 +2,12 @@ from pymel.all import *
 
 
 class PanelWindow( object ):
-   
+    
+    gSampleState=[]
     
     def __init__( self, name, title, namespace=__name__ ):
+        
+        print "self instanceName: %s " % self
         self.__name__ = name
         self._title = title
 
@@ -14,23 +17,23 @@ class PanelWindow( object ):
             scriptedPanelType( self.__name__, unique=True )
         print self.instance 
         jobCmd = 'python(\\\"%s._setup()\\\")' % self.instance
-        job = "import %s ; scriptJob -replacePrevious -parent \"%s\" -event \"SceneOpened\" \"%s\";" % (namespace, self.__name__, jobCmd )
+        job = "scriptJob -replacePrevious -parent \"%s\" -event \"SceneOpened\" \"%s\";" % ( self.__name__, jobCmd )
         mel.eval(job)
 
-        scriptedPanelType( self.__name__, edit=True,
+        result = scriptedPanelType( self.__name__, edit=True,
                            unique=True,
-                           createCallback='python("%s._createCallback()")' % self.instance,
-                           initCallback='python("%s._initCallback()"  )' % self.instance,
-                           addCallback='python("%s._addCallback()"   )' % self.instance,
-                           removeCallback='python("%s._removeCallback()")' %  self.instance,
-                           deleteCallback='python("%s._deleteCallback()")' % self.instance,
-                           saveStateCallback='python("%s._deleteCallback()")' % self.instance
+                           createCallback='python("import %s ; print \\"callback: %s._createCallback()\\" ; %s._createCallback()")' % (namespace, self.instance, self.instance),
+                           initCallback='python("import %s ; %s._initCallback()"  )' % (namespace, self),
+                           addCallback='python("import %s ; print \\"callback: %s._createCallback()\\" ; %s._addCallback()"   )' % (namespace, self.instance, self.instance),
+                           removeCallback='python("import %s ; %s._removeCallback()")' % (namespace, self.instance),
+                           deleteCallback='python("import %s ; %s._deleteCallback()")' % (namespace, self.instance),
+                           saveStateCallback='python("import %s ; %s._deleteCallback()")' % (namespace, self.instance)
                            )
-
+        print "RESULT: %s " % result
 
     def _setup(self):
         """Command to be call for new scene"""
-
+        print 'SETUP CALLED'
         panelName = sceneUIReplacement( getNextScriptedPanel=(self.__name__, self._title) )
 
         if panelName == '':
@@ -59,11 +62,49 @@ class PanelWindow( object ):
     def _addCallback(self):
         """Create UI and parent any editors."""
         print 'ADD CALLBACK'
+        #
+        #  Description:  Create UI and parent any editors.
+        #
+        columnLayout('topCol',adj=True)
+        separator(h=10,style="none")
+        frameLayout(mw=10,l="Sliders")
+        columnLayout('sampleCol',adj=True)
+        separator(h=10,style="none")
+        floatSliderGrp('fsg1',v=gSampleState[0],
+            l="Property A",f=True)
+        floatSliderGrp('fsg2',v=gSampleState[1],
+            l="Property B",f=True)
+        floatSliderGrp('fsg3',v=gSampleState[2],
+            l="Property C",f=True)
+        separator(h=10,style="none")
+        setParent('..')
+        setParent('..')
+        separator(h=10,style="none")
+        frameLayout(mw=10,l="Radio Buttons")
+        columnLayout('sampleCol2')
+        separator(h=10,style="none")
+        radioButtonGrp('rbg',nrb=3,
+            l="Big Options",
+            select=gSampleState[3],
+            la3=("Option 1", "Option 2", "Option 3"))
+        radioButtonGrp('rbg2',nrb=3,
+            l="Little Options",
+            select=gSampleState[4],
+            la3=("Option 4", "Option 5", "Option 6"))
+        separator(h=10,style="none")
 
 
     def _removeCallback(self):
         """Unparent any editors and save state if required."""
         print 'REMOVE CALLBACK'
+        control=str(scriptedPanel(panelName,
+                                  q=1,control=1))
+        setParent(control)
+        gSampleState[0]=float(floatSliderGrp('fsg1',q=1,v=1))
+        gSampleState[1]=float(floatSliderGrp('fsg2',q=1,v=1))
+        gSampleState[2]=float(floatSliderGrp('fsg3',q=1,v=1))
+        gSampleState[3]=float(radioButtonGrp('rbg',q=1,sl=1))
+        gSampleState[4]=float(radioButtonGrp('rbg2',q=1,sl=1))
 
 
     def _deleteCallback(self):
@@ -74,9 +115,10 @@ class PanelWindow( object ):
     def _saveCallback(self):
         """Save Callback."""
         print 'SAVE CALLBACK'
-        reCreateCommand = ''
+        indent="\n\t\t\t"
+        reCreateCommand = str(indent) + "$gSampleState[0]=" + str(melGlobals['gSampleState'][0]) + ";" + str(indent) + "$gSampleState[1]=" + str(melGlobals['gSampleState'][1]) + ";" + str(indent) + "$gSampleState[2]=" + str(melGlobals['gSampleState'][2]) + ";" + str(indent) + "$gSampleState[3]=" + str(melGlobals['gSampleState'][3]) + ";" + str(indent) + "$gSampleState[4]=" + str(melGlobals['gSampleState'][4]) + ";" + str(indent) + "setSamplePanelState $panelName;\n"
         return reCreateCommand
-
+        
 
     def show( self ):
         #scriptedPanel( self._title, edit=True, tearOff=True )
@@ -84,6 +126,7 @@ class PanelWindow( object ):
 
 
 def openTestPanel():
-    global testPanel
-    testPanel = PanelWindow( 'testPanel','testPanelobj' )
+    global testPanel 
+    testPanel = PanelWindow( 'testPanel','testPanelObj' )
     testPanel.show()
+    return testPanel
