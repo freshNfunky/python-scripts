4fa260fe7a6c575ef7964bc814e63c47114818e7
freshNfunky <felix@ftc-creative.de>
2012-09-07 18:05:41 +0200

    new test program

--- python/panelWindowCallupTst.py
+++ python/panelWindowCallupTst.py
@@ -3,9 +3,12 @@ import pymel  # touch pat.py in the cwd and try to import the empty file under L
 import string 
 import os,sys
 
+
 if not os.getcwd() in sys.path:
     sys.path.append(os.getcwd())
-import panelWindow as pw
 
-reload (pw)
-pw.openTestPanel()
+import panelWindow    
+# from panelWindow import *
+reload(panelWindow)
+Obj = panelWindow.openTestPanel()
+# print Obj
